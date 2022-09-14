8123a35f227fe835798e91e16bd08351814b316e
khalidzein83 <khalid.zein1@gmail.com>
2022-09-13 20:01:38 -0400

    finsihed uploading the data to the tables

--- knex/seed_data/products_data.js
+++ knex/seed_data/products_data.js
@@ -1,8 +1,38 @@
 module.exports = [
     {
+        product_name: "Lisa Straight-Leg Flat Front Trouser",
+        product_image: "http://localhost:8080/image/trouser_pants1.jpg",
+        product_brand: "Olsen",
+        price: 149,
+    },
+    {
         product_name: "Izzy Mid-Rise Raw Edge Flare Jeans",
-        product_image: "http://localhost:8080/image/jeans_pants1.jpg",
+        product_image: "http://localhost:8080/image/jeans_jeans1.jpg",
         product_brand: "Free People",
         price: 198,
+    },
+    {
+        product_name: "Lisa-Fit Straight-Leg Jersey Pants",
+        product_image: "http://localhost:8080/image/pants_pants1.jpg",
+        product_brand: "Olsen",
+        price: 149,
+    },
+    {
+        product_name: "Seersucker Mini Check Flared Pants",
+        product_image: "http://localhost:8080/image/check_pants1.jpg",
+        product_brand: "GANNI",
+        price: 325,
+    },
+    {
+        product_name: "Eros Cotton Twill Utility Trousers",
+        product_image: "http://localhost:8080/image/trouser_util1.jpg",
+        product_brand: "Weekend Max Mara",
+        price: 415,
+    },
+    {
+        product_name: "Straight-Leg Smart Trousers",
+        product_image: "http://localhost:8080/image/smart_pants1.jpg",
+        product_brand: "River Island",
+        price: 80,
     },
 ];
